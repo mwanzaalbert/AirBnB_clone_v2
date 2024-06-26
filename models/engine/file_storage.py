dafb286167513846a7db518f2586ee4021c3449f
mwanzaalbert <user.email>
2024-06-26 18:17:31 +0300

    updated

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -31,14 +31,14 @@ class FileStorage:
     def new(self, obj):
         """Add new object to storage dictionary."""
         key = f"{obj.__class__.__name__}.{obj.id}"
-        FileStorage.__objects[key] = obj
+        self.__objects[key] = obj
 #         self.__objects.update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
 
     def save(self):
         """Save storage dictionary to file."""
         with open(FileStorage.__file_path, 'w') as f:
             temp = {key: val.to_dict() for key,
-                    val in FileStorage.__objects.items()}
+                    val in self.__objects.items()}
             json.dump(temp, f)
 
     def reload(self):
@@ -62,7 +62,7 @@ class FileStorage:
                 for key, val in temp.items():
                     cls_name = val['__class__']
                     if cls_name in classes:
-                        self.all()[key] = classes[cls_name](**val)
+                        self.__objects[key] = classes[cls_name](**val)
                         
         except FileNotFoundError:
             pass
@@ -74,6 +74,7 @@ class FileStorage:
             key = f"{obj.__class__.__name__}.{obj.id}"
             if key in FileStorage.__objects:
                 del self.__objects[key]
+                self.save()
 
     def close(self):
         """Call the reload for deserializing the JSON file to objects."""
