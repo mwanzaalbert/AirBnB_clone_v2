27fa6f038aaee6d3d6d8e98f2cd1cc3ae52414c4
mwanzaalbert <user.email>
2024-06-26 17:14:15 +0300

    docstrings update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""This module defines a class to manage file storage for hbnb clone"""
+"""Module defines a class to manage file storage for hbnb clone."""
 import json
 
 
 class FileStorage:
-    """This class manages storage of hbnb models in JSON format"""
+    """Manage storage of hbnb models in JSON format."""
     __file_path = 'file.json'
     __objects = {}
 
     def all(self, cls=None):
         """
-        Returns a dictionary of models currently in storage.
+        Return a dictionary of models currently in storage.
+        
         If a class is specified, return a dictionary of objects of that class.
         """
         if cls:
@@ -19,18 +20,18 @@ class FileStorage:
         return self.__objects.__objects
 
     def new(self, obj):
-        """Adds new object to storage dictionary"""
+        """Add new object to storage dictionary."""
         self.__objects.update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
 
     def save(self):
-        """Saves storage dictionary to file"""
+        """Save storage dictionary to file."""
         with open(FileStorage.__file_path, 'w') as f:
             temp = {key: val.to_dict() for key,
                     val in FileStorage.__objects.items()}
             json.dump(temp, f)
 
     def reload(self):
-        """Loads storage dictionary from file"""
+        """Load storage dictionary from file."""
         from models.base_model import BaseModel
         from models.user import User
         from models.place import Place
@@ -53,12 +54,12 @@ class FileStorage:
             pass
 
     def delete(self, obj=None):
-        """Removes object from storage dictionary"""
+        """Remove object from storage dictionary."""
         if obj:
             key = obj.to_dict()['__class__'] + '.' + obj.id
             if key in FileStorage.__objects:
                 del self.__objects.__objects[key]
 
     def close(self):
-        """Call the reload method for deserializing the JSON file to objects"""
+        """Call the reload method for deserializing the JSON file to objects."""
         self.reload()
